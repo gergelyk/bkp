eca49239dc3a19d4a50b5f2fb4b496759c076c0a
Grzegorz Krason <grzegorz.krason@nokia.com>
2018-11-25 21:42:58 +0100

    update pypi long description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+readme_path = Path(__file__).parent / 'README.rst'
+
+with open(readme_path) as fh:
+    long_description = fh.read()
 
 setup(
     name = 'bkp',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Creates backups of your files and directories.',
     author = 'Grzegorz Krason',
     author_email = 'grzegorz.krason@gmail.com',
@@ -10,6 +16,8 @@ setup(
     license = 'MIT',
     packages = find_packages(),
     keywords = 'backup'.split(),
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     entry_points = {
         'console_scripts': ['bkp = bkp:main'],
         },
